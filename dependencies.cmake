4840283d46cbef23775d2a2a803e7a86e9000693
skysw <skwa00001@stud.uni-saarland.de>
2026-06-17 21:55:47 +0200

    add yaml-cpp for shader parsing

--- dependencies.cmake
+++ dependencies.cmake
@@ -3,6 +3,7 @@ find_package(glad REQUIRED)
 find_package(libassert REQUIRED)
 find_package(glm REQUIRED)
 find_package(glfw3 REQUIRED)
+find_package(yaml-cpp REQUIRED)
 
 target_link_libraries(2iren
         glad::glad
@@ -10,4 +11,5 @@ target_link_libraries(2iren
         libassert::assert
         glm::glm
         glfw
+        yaml-cpp::yaml-cpp
 )
